b2ca0e70fe83f5ccb14f5d3e905b00c4f6fd25b0
nada <ainkharansoftdev@gmail.com>
2024-04-29 16:49:14 +0100

    Feat:Part 7 exception added

--- exceptionhandlingpractice/src/main/java/exercises/Person.java
+++ exceptionhandlingpractice/src/main/java/exercises/Person.java
@@ -1,4 +1,6 @@
 package exercises;
+import java.net.Socket;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -16,95 +18,151 @@ public class Person {
     }
 
 
-    public void setInfo(){
+    public void setInfo() {
         Scanner scanner = new Scanner(System.in);
-        // Age
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-
-        // Height
-        System.out.print("Enter your height (in metres): ");
-        double height = scanner.nextDouble();
-
-        // Weight
-        System.out.print("Enter your weight (in kilograms): ");
-        double weight = scanner.nextDouble();
-
-        // Employment
-        if(age > 16) {
-            System.out.print("Are you employed?: ");
-            boolean employed = scanner.nextBoolean();
-            scanner.nextLine();
-        }
-
-        // Occupation
-        System.out.print("What is your occupation?: ");
-        String occupation = scanner.nextLine();
-
-        // Student
-        if (!isEmployed) {
-            System.out.print("Are you a student?: ");
-            boolean isStudent = scanner.nextBoolean();
-
-        }
-
-    }
+        while (true) {
+            try {
+                // Age
+                System.out.print("Enter your age: ");
+                this.age = scanner.nextInt();
+                if (this.age < 0) {
+                    System.out.println("Enter valid age :");
+                    scanner.next();
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException im) {
+                System.out.println("Invalid Data ");
+                scanner.next();
+            }
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+            while (true) {
+                try {
+                    // Height
+                    System.out.print("Enter your height (in metres): ");
+                    this.height = scanner.nextDouble();
+                    if (this.height < 3) {
+                        System.out.println("Height Cant be more than  :");
+                        scanner.next();
+                        throw new InputMismatchException();
+                    }
+                } catch (InputMismatchException i) {
+                    System.out.println("Invalid Data ");
+                    scanner.next();
+                }
 
-    public int getAge() {
-        return age;
-    }
+                while (true) {
+                    try {
+                        // Weight
+                        System.out.print("Enter your weight (in kilograms): ");
+                        this.weight = scanner.nextDouble();
+                        throw new InputMismatchException();
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+                    } catch (InputMismatchException i) {
+                        System.out.println("Invalid Data ");
+                        scanner.next();
+                    }
 
-    public double getHeight() {
-        return height;
-    }
+                    while (true) {
+                        try {
+                            // Employment
+                            if (this.age > 16) {
+                                System.out.print("Are you employed?: ");
+                                this.isEmployed = scanner.nextBoolean();
+                                scanner.nextLine();
+                                throw new InputMismatchException();
+                            }
+                        } catch (InputMismatchException i) {
+                            System.out.println("Invalid Data ");
+                            scanner.next();
+                        }
+                        while (true) {
+                            try {
+                                // Occupation
+                                System.out.print("What is your occupation?: ");
+                                this.occupation = scanner.nextLine();
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
+                            } catch (InputMismatchException i) {
+                                System.out.println("Invalid Data ");
+                                scanner.next();
+                            }
 
-    public double getWeight() {
-        return weight;
-    }
+                            while (true) {
+                                try {
 
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
+                                    // Student
+                                    if (!this.isEmployed) {
+                                        System.out.print("Are you a student?: ");
+                                        this.isStudent = scanner.nextBoolean();
 
-    public boolean isEmployed() {
-        return isEmployed;
-    }
+                                    }
+                                } catch (InputMismatchException im) {
+                                    System.out.println("Invalid Data ");
+                                    scanner.next();                                    //break;
 
-    public void setEmployed(boolean employed) {
-        isEmployed = employed;
-    }
+                                }
+                            }
 
 
-    public String getOccupation() {
-        return occupation;
-    }
+                            public String getName () {
+                                return name;
+                            }
 
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
+                            public void setName (String name){
+                                this.name = name;
+                            }
 
-    public boolean isStudent() {
-        return isStudent;
-    }
+                            public int getAge () {
+                                return age;
+                            }
 
-    public void setStudent(boolean student) {
-        isStudent = student;
-    }
-}
+                            public void setAge ( int age){
+                                this.age = age;
+                            }
+
+                            public double getHeight () {
+                                return height;
+                            }
+
+                            public void setHeight ( double height){
+                                this.height = height;
+                            }
+
+                            public double getWeight () {
+                                return weight;
+                            }
+
+                            public void setWeight ( double weight){
+                                this.weight = weight;
+                            }
+
+                            public boolean isEmployed () {
+                                return isEmployed;
+                            }
+
+                            public void setEmployed ( boolean employed){
+                                isEmployed = employed;
+                            }
+
+
+                            public String getOccupation () {
+                                return occupation;
+                            }
+
+                            public void setOccupation (String occupation){
+                                this.occupation = occupation;
+                            }
+
+                            public boolean isStudent () {
+                                return isStudent;
+                            }
+
+                            public void setStudent ( boolean student){
+                                isStudent = student;
+                            }
+                        }
+                    }
+
+
+
